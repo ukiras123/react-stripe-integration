bfb50d4820395db05c6366e176af4e1a47b9d6a5
ukiras123 <ukiras@gmail.com>
2023-08-16 16:21:18 +1000

    add checkout api

---
+++ api/index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const app = express();
+var cors = require("cors");
+
+require("dotenv").config();
+
+const { STRIPE_SECRET_KEY, CLIENT_URL } = process.env;
+// @ts-ignore
+const stripe = require("stripe")(STRIPE_SECRET_KEY);
+const PORT = 8080;
+
+app.use(express.json());
+app.use(cors());
+app.get("/health", (req, res) => {
+  res.status(200).send({
+    status: "health",
+  });
+});
+
+app.post("/create-checkout-session", async (req, res) => {
+  console.log("Creating stripe checkout session");
+  try {
+    const productDetails = req.body;
+    const lineItems = productDetails.map((p) => ({
+      price_data: {
+        // The currency parameter determines which
+        // payment methods are used in the Checkout Session.
+        currency: "aud",
+        product_data: {
+          name: p.title,
+          description: p.description,
+          images: [p.img],
+        },
+        unit_amount: p.price * 100,
+      },
+      quantity: p.quantity,
+    }));
+    const session = await stripe.checkout.sessions.create({
+      line_items: lineItems,
+      mode: "payment",
+      success_url: `${CLIENT_URL}?success=true`,
+      cancel_url: `${CLIENT_URL}?success=false`,
+    });
+    res.status(200).send(session);
+  } catch (e) {
+    res.status(500).send(`Something went wrong ${e}`);
+    console.log("Error", e);
+  }
+});
+app.listen(PORT, () =>
+  console.log(`Server is live in http://localhostL=:${PORT}`)
+);
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,29 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+
+import "react-toastify/dist/ReactToastify.css";
 import Card from "../../components/Card/Card";
 import data from "../../data/products.json";
 
+import { useDispatch } from "react-redux";
+import { resetCart } from "../../redux/cartReducer";
 import "./Home.scss";
 const Home = () => {
-  return (
-    <div className="products">
-      <img
-        src="https://images.pexels.com/photos/1074535/pexels-photo-1074535.jpeg"
-        alt="feature img"
-        className="featureImg"
-      />
-      <div className="content">
-        <div className="title">
-          <h1>Select Your Style</h1>
-          <div className="line"></div>
-        </div>
+  let [searchParams] = useSearchParams();
+  const [isSuccess, setIsSuccess] = useState(null);
+  const isSuccessAfterPayment = searchParams.get("success");
 
-        <div className="productLists">
-          {data?.products.map((product) => (
-            <Card key={product.id} item={product} />
-          ))}
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (isSuccessAfterPayment == "true") {
+      dispatch(resetCart());
+      // @ts-ignore
+      setIsSuccess(true);
+    }
+    if (isSuccessAfterPayment == "false") {
+      // @ts-ignore
+      setIsSuccess(false);
+    }
+  }, [isSuccessAfterPayment]);
+
+  useEffect(() => {
+    if (isSuccess === null) return;
+    if (isSuccess) {
+      toast.success("Your order is placed successfully!");
+    } else {
+      toast.error("Something went wrong. Please try again!");
+    }
+  }, [isSuccess]);
+  return (
+    <>
+      <div className="products">
+        <img
+          src="https://images.pexels.com/photos/1074535/pexels-photo-1074535.jpeg"
+          alt="feature img"
+          className="featureImg"
+        />
+        <div className="content">
+          <div className="title">
+            <h1>Select Your Style</h1>
+            <div className="line"></div>
+          </div>
+
+          <div className="productLists">
+            {data?.products.map((product) => (
+              <Card key={product.id} item={product} />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+      <ToastContainer />
+    </>
   );
 };
 
